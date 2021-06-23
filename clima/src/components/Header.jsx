7873f1b6443e262c9a06874e4d9ef4e3a1996fee
humberto <humbertorivero9@gmail.com>
2021-06-23 20:16:15 -0300

    componente de header

---
+++ clima/src/components/Header.jsx
@@ -0,0 +1,9 @@
+export default function Header({title}) {
+    return (
+        <nav>
+            <div className="nav-wrapper light-blue darken-2">
+                <a href="#!" className="brand-logo">{title}</a>
+            </div>
+        </nav>
+    )
+}
